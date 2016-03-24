668be498bff64eb748c797f5473e9342fd6f4f3b
Lian Wang <kohlemo@gmail.com>
2016-03-25 01:54:08 +1300

    Customer - list

--- src/SAKURA.NZB.Website/wwwroot/src/app/components/customers/customers.component.js
+++ src/SAKURA.NZB.Website/wwwroot/src/app/components/customers/customers.component.js
@@ -1,4 +1,5 @@
-System.register(["angular2/core", "angular2/src/common/directives/core_directives", "../api.service"], function(exports_1) {
+/// <reference path="../../../../lib/TypeScript-Linq/Scripts/typings/System/Collections/Generic/List.ts" />
+System.register(["angular2/core", "angular2/common", "../api.service", '../../../../lib/TypeScript-Linq/Scripts/System/Collections/Generic/List.js'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,32 +9,47 @@ System.register(["angular2/core", "angular2/src/common/directives/core_directive
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, core_directives_1, api_service_1;
-    var CustomersComponent;
+    var core_1, common_1, api_service_1;
+    var Customer, CustomersComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (core_directives_1_1) {
-                core_directives_1 = core_directives_1_1;
+            function (common_1_1) {
+                common_1 = common_1_1;
             },
             function (api_service_1_1) {
                 api_service_1 = api_service_1_1;
-            }],
+            },
+            function (_1) {}],
         execute: function() {
+            Customer = (function () {
+                function Customer(obj) {
+                    this.id = obj.Id;
+                    this.name = obj.FullName;
+                    this.tel = obj.Phone1;
+                    this.address = obj.Address;
+                }
+                return Customer;
+            })();
+            exports_1("Customer", Customer);
             CustomersComponent = (function () {
                 function CustomersComponent(service) {
                     this.service = service;
+                    this.icons = ['ambulance', 'car', 'bicycle', 'bus', 'taxi', 'car', 'fighter-jet', 'motorcycle', 'plane', 'rocket', 'ship', 'space-shuttle', 'subway', 'taxi', 'train', 'truck'];
+                    this.customerList = [];
                 }
                 CustomersComponent.prototype.ngOnInit = function () {
                     this.get();
                 };
                 CustomersComponent.prototype.get = function () {
-                    var _this = this;
+                    var that = this;
                     this.service.getCustomers(function (json) {
                         if (json) {
-                            _this.data = JSON.stringify(json);
+                            json.forEach(function (c) {
+                                that.customerList.push(new Customer(c));
+                            });
                         }
                     });
                 };
@@ -42,7 +58,7 @@ System.register(["angular2/core", "angular2/src/common/directives/core_directive
                         selector: "customers",
                         templateUrl: "/partial/customers",
                         providers: [api_service_1.ApiService],
-                        directives: core_directives_1.CORE_DIRECTIVES
+                        directives: [common_1.CORE_DIRECTIVES, common_1.FORM_DIRECTIVES]
                     }), 
                     __metadata('design:paramtypes', [api_service_1.ApiService])
                 ], CustomersComponent);
--- src/SAKURA.NZB.Website/wwwroot/src/app/components/customers/customers.component.ts
+++ src/SAKURA.NZB.Website/wwwroot/src/app/components/customers/customers.component.ts
@@ -1,15 +1,35 @@
-﻿import {Component, OnInit} from "angular2/core";
-import {CORE_DIRECTIVES} from "angular2/src/common/directives/core_directives";
+﻿/// <reference path="../../../../lib/TypeScript-Linq/Scripts/typings/System/Collections/Generic/List.ts" />
+
+import {Component, OnInit} from "angular2/core";
+import {CORE_DIRECTIVES, FORM_DIRECTIVES} from "angular2/common";
 import {ApiService} from "../api.service";
+
+import '../../../../lib/TypeScript-Linq/Scripts/System/Collections/Generic/List.js';
+
+export class Customer {
+	id: number;
+	name: string;
+	tel: string;
+	address: string;
+
+	constructor(obj) {
+		this.id = obj.Id;
+		this.name = obj.FullName;
+		this.tel = obj.Phone1;
+		this.address = obj.Address;
+	}
+}
+
 
 @Component({
     selector: "customers",
     templateUrl: "/partial/customers",
     providers: [ApiService],
-    directives: CORE_DIRECTIVES
+    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
 export class CustomersComponent implements OnInit {
-    data: any;
+	icons = ['ambulance', 'car', 'bicycle', 'bus', 'taxi', 'car', 'fighter-jet', 'motorcycle', 'plane', 'rocket', 'ship', 'space-shuttle', 'subway', 'taxi', 'train', 'truck'];
+    customerList: Customer[] = [];
 
     constructor(private service: ApiService) { }
 
@@ -18,9 +38,13 @@ export class CustomersComponent implements OnInit {
     }
 
     get() {
+		var that = this;
+
         this.service.getCustomers(json => {
             if (json) {
-                this.data = JSON.stringify(json);
+                json.forEach(c => {
+					that.customerList.push(new Customer(c));
+				});
 
             }
         });
