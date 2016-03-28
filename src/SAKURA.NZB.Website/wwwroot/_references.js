e79dcc58c7a61bbf1b0d475d65679a263edd7cfc
Lian Wang <kohlemo@gmail.com>
2016-03-28 20:30:25 +1300

    Products - link to base info edit page (mobile)

--- src/SAKURA.NZB.Website/wwwroot/_references.js
+++ src/SAKURA.NZB.Website/wwwroot/_references.js
@@ -24,6 +24,7 @@
 /// <reference path="src/app/components/customers/list.component.js" />
 /// <reference path="src/app/components/mvc.component.js" />
 /// <reference path="src/app/components/products/baseedit.component.js" />
+/// <reference path="src/app/components/products/baseedit.js" />
 /// <reference path="src/app/components/products/list.component.js" />
 /// <reference path="src/app/components/products/models.js" />
 /// <reference path="src/app/components/static.component.js" />
