d3c774bc97e8bab768da4dec87bcee5a5813babf
Lian Wang <kohlemo@gmail.com>
2016-03-31 20:02:30 +1300

    Exchange rates web api

---
+++ src/SAKURA.NZB.Website/Controllers/API/ExchangeRatesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.AspNet.Mvc;
+using SAKURA.NZB.Data;
+
+namespace SAKURA.NZB.Website.Controllers.API
+{
+	[Route("api/[controller]")]
+    public class ExchangeRatesController : Controller
+    {
+		private NZBContext _context;
+
+		public ExchangeRatesController(NZBContext context)
+		{
+			_context = context;
+		}
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new ObjectResult(_context.ExchangeRates.ToList());
+        }
+
+        [HttpGet, Route("latest")]
+        public IActionResult Get(int id)
+        {
+			return new ObjectResult(_context.ExchangeRates.OrderByDescending(e => e.ModifiedTime).FirstOrDefault());
+		}
+    }
+}
