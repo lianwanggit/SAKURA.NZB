bb4f02746d5ff04cb21cf83dc888b349a0fb8360
Lian Wang <kohlemo@gmail.com>
2016-04-11 23:53:28 +1200

    Order - edit page adds customer indexer

--- src/SAKURA.NZB.Website/wwwroot/src/app/components/orders/edit.component.ts
+++ src/SAKURA.NZB.Website/wwwroot/src/app/components/orders/edit.component.ts
@@ -4,7 +4,8 @@ import {Component, OnInit} from "angular2/core";
 import {CORE_DIRECTIVES, FORM_DIRECTIVES, ControlGroup, Control, Validators} from "angular2/common";
 import {Router, RouteParams, ROUTER_DIRECTIVES} from 'angular2/router';
 import {ApiService} from "../api.service";
-
+import {AlphaIndexerDirective, Element} from "../../directives/alphaIndexer.directive";
+import {Customer} from "../customers/edit.component";
 import {SelectValidator, ValidationResult} from "../../validators/selectValidator";
 
 import '../../../../lib/TypeScript-Linq/Scripts/System/Collections/Generic/List.js';
@@ -14,11 +15,14 @@ import '../../../../lib/TypeScript-Linq/Scripts/System/Collections/Generic/List.
     templateUrl: "./src/app/components/orders/edit.html",
 	styleUrls: ["./src/app/components/orders/orders.css"],
     providers: [ApiService],
-    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, ROUTER_DIRECTIVES]
+    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, ROUTER_DIRECTIVES, AlphaIndexerDirective]
 })
 export class OrderEditComponent implements OnInit {
 	private editMode = false;
 	orderId: string;
+
+	elementSource: Element[];
+	selectedCustomer: Customer;
 
 	constructor(private service: ApiService, private router: Router, params: RouteParams) {
 		this.orderId = params.get("id");
@@ -29,6 +33,37 @@ export class OrderEditComponent implements OnInit {
 
 	ngOnInit() {
 		var that = this;
+		this.getCustomers();
+	}
+
+	onElementSelected(id: string) {
+		this.getCustomer(id);
+	}
+
+	getCustomer(id: string) {
+		var that = this;
+
+        this.service.getCustomer(id, json => {
+            if (json) {
+                that.selectedCustomer = new Customer(json);
+            }
+        });
+	}
+
+	getCustomers() {
+		var that = this;
+
+		this.service.getCustomers(json => {
+			if (json) {
+				var list = [].ToList<Element>();
+				json.forEach(x => {
+					var c = new Customer(x);
+					list.Add(new Element(c.id, c.fullName, c.namePinYin));
+				});
+
+				that.elementSource = list.ToArray();
+			}
+		});
 	}
 
 	get title() { return this.editMode ? "编辑订单 " : "新建订单"; }
